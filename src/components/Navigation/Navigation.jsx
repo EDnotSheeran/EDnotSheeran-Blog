2cba3db6d95d30b173042662975594d0f59a7e8e
EDnotSheeran <juninho.tab3@gmail.com>
2020-07-13 22:30:05 -0300

    navigation implemented

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Avatar } from 'antd'
+
+const navLinks = [
+    {
+        title: 'home',
+        path: '/'
+    },
+    {
+        title: 'blog',
+        path: '/blog'
+    },
+    {
+        title: 'contact us',
+        path: '/contact-us'
+    },
+    {
+        title: 'login',
+        path: '/login'
+    }
+]
+
+export default ({user}) => {
+    const [menuActive, setMenuActive] = useState(false)
+
+    return (
+        <nav className='site-navigation'>
+            <span className='menu-title'>My Blog React</span>
+            <div className={`menu-content-container ${menuActive && 'active'}`}>
+                <ul>
+                    {navLinks.map((link, index) => (
+                        <li key={index}>
+                            <Link to={link.path}>{link.title}</Link>
+                        </li>
+                    ))
+                    }
+                </ul>
+                <span className='menu-avatar-container'>
+                    <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" size={38}/>
+                    <span className='menu-avatar-name'>{`${user.firstName} ${user.lastName}`}</span>
+                </span>
+            </div>
+            <i className='ionicons icon ion-ios-menu' onClick={() => setMenuActive(!menuActive)}/>
+        </nav>
+    )
+}
