249e3b8eea11b9182b12b440024190d283c2bbe2
EDnotSheeran <juninho.tab3@gmail.com>
2020-07-14 17:59:22 -0300

    Updates

---
+++ precache-manifest.6e3ba14331ae28b9741286c025759343.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "050eefedb09d58095811da5d5389c1c0",
+    "url": "/EDnotSheeran-Blog/index.html"
+  },
+  {
+    "revision": "b25cedc1b9180f0e25d4",
+    "url": "/EDnotSheeran-Blog/static/css/2.aca3de8c.chunk.css"
+  },
+  {
+    "revision": "bbf33444ec09abceff02",
+    "url": "/EDnotSheeran-Blog/static/css/main.96731f64.chunk.css"
+  },
+  {
+    "revision": "b25cedc1b9180f0e25d4",
+    "url": "/EDnotSheeran-Blog/static/js/2.c9eec9f9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/EDnotSheeran-Blog/static/js/2.c9eec9f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbf33444ec09abceff02",
+    "url": "/EDnotSheeran-Blog/static/js/main.2b432436.chunk.js"
+  },
+  {
+    "revision": "3cef967da507128ef728",
+    "url": "/EDnotSheeran-Blog/static/js/runtime-main.fa6ac6fe.js"
+  },
+  {
+    "revision": "96f1c901c087fb64019f7665f7f8aca6",
+    "url": "/EDnotSheeran-Blog/static/media/ionicons.96f1c901.woff2"
+  },
+  {
+    "revision": "99b863497156d4478ec3489fefb3815c",
+    "url": "/EDnotSheeran-Blog/static/media/ionicons.99b86349.woff"
+  }
+]);
